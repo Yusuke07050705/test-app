e4db86595d29902639fea500b5ef030eee16e85e
Yusuke Satomi <yusuke07.0507@gmail.com>
2025-05-07 17:20:26 +0900

    test-commit

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import TaskOne from './taskOne';
-import { posts } from './data/posts';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <TaskOne data={posts} />
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
